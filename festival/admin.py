90e7cab74529b7562b7b8dbbc87025688d01b0d0
LeeJaesung2 <dlwotjd9909@naver.com>
2022-09-20 20:31:34 +0900

    add static folder

--- festival/admin.py
+++ festival/admin.py
@@ -2,9 +2,9 @@ from getpass import GetPassWarning
 from django.contrib import admin
 
 # Register your models here.'
-from .models import Booth, Food, Menu, Gongzi
+from .models import Booth, Food, Menu, Notice
 
 admin.site.register(Booth)
 admin.site.register(Food)
 admin.site.register(Menu)
-admin.site.register(Gongzi)
+admin.site.register(Notice)
