d7184d21b2e027433bf5639a7a0d131d158093d5
kimminji000 <minji6764@naver.com>
2022-09-25 02:50:56 +0900

    .

--- festival/urls.py
+++ festival/urls.py
@@ -2,13 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('booths', views.getBooths, name="Booths"),
-    path('booths2', views.getBooths2, name="Booths2"),
-    #path('booths2/<int:booth_id>', views.getBoothById, name="getBoothById"),
-    path('booth/<int:booth_id>/event', views.getBoothEvents, name="getBoothEvents"),
-    path('foods', views.getFoods, name="getFoods"),
-    path('food/<int:food_id>', views.getFoodById, name="getFoodById"),
-    path('food/<int:food_id>/menu', views.getFoodMenus, name="getFoodMenus"),
-    path('notices', views.getNotices, name="getNotices"),
-    path('notice/<int:notice_id>', views.getNoticeById, name="getNoticeById"),    
+    path('navbar', views.navbar, name="navbar"),
+    path('booth', views.booth, name="booth"),
+    path('booth2', views.booth2, name="booth2"),
+    path('food', views.food, name="food"),
+    path('info', views.info, name="info"),   
 ]
--- festival/views.py
+++ festival/views.py
@@ -6,34 +6,21 @@ from .models import Booth, BoothEvent, Food, Menu, Notice
 def home(request):
     return render(request,'main.html')
 
-def getBooths(request):
+def navbar(request):
+    return render(request,'navbar.html')
+
+def booth(request):
     booths = Booth.objects.all()
     return render(request, 'booth.html', {'booths' : booths})
 
-def getBooths2(request):
+def booth2(request):
     booths = Booth.objects.all()
     return render(request, 'booth2.html', {'booths' : booths})
 
-def getBoothEvents(request, booth_id):
-    boothEvents = BoothEvent.objects.filter(booth = booth_id)
-    return render(request, 'booth.html', {'boothEvents' : boothEvents})
-
-def getFoods(request):
+def food(request):
     foods = Food.objects.all()
     return render(request, 'food.html', {'foods' : foods})
 
-def getFoodById(request, food_id):
-    food = Food.objects.get(pk = food_id)
-    return render(request, 'food.html', {'food' : food})
-
-def getFoodMenus(request, food_id):
-    menu = Menu.objects.filter(name = food_id)
-    return render(request, 'food.html', {'menu' : menu})
-
-def getNotices(request):
+def info(request):
     notices = Notice.objects.all()
     return render(request, 'notice.html', {'notices' : notices})
-
-def getNoticeById(request, notice_id):
-    notice = Notice.objects.get(pk = notice_id)
-    return render(request, 'notice.html', {'notice' : notice})
