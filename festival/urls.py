e335251f68ba01ec3ca13636fe49b402d501a24f
kimminji000 <minji6764@naver.com>
2022-09-27 19:51:23 +0900

    template

--- festival/urls.py
+++ festival/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    path('navbar', views.navbar, name="navbar"),
+urlpatterns = [    
     path('booth', views.booth, name="booth"),
     path('booth2', views.booth2, name="booth2"),
+    path('dday', views.dday, name="dday"),
     path('food', views.food, name="food"),
-    path('info', views.info, name="info"),   
+    path('info', views.info, name="info"),
+    path('navbar', views.navbar, name="navbar"),
+    path('schedule', views.schedule, name="schedule"),
 ]
--- festival/views.py
+++ festival/views.py
@@ -6,21 +6,29 @@ from .models import Booth, BoothEvent, Food, Menu, Notice
 def home(request):
     return render(request,'main.html')
 
-def navbar(request):
-    return render(request,'navbar.html')
-
 def booth(request):
     booths = Booth.objects.all()
     return render(request, 'booth.html', {'booths' : booths})
 
 def booth2(request):
     booths = Booth.objects.all()
-    return render(request, 'booth2.html', {'booths' : booths})
+    events = BoothEvent.objects.all()
+    return render(request, 'booth2.html', {'booths' : booths, 'events' : events})
+
+def dday(request):
+    return render(request,'dday.html')
 
 def food(request):
     foods = Food.objects.all()
-    return render(request, 'food.html', {'foods' : foods})
+    menus = Menu.objects.all()
+    return render(request, 'food.html', {'foods' : foods, 'menus' : menus})
 
 def info(request):
     notices = Notice.objects.all()
-    return render(request, 'notice.html', {'notices' : notices})
+    return render(request, 'info.html', {'notices' : notices})
+
+def navbar(request):
+    return render(request,'navbar.html')
+
+def schedule(request):
+    return render(request,'schedule.html')    
