2fe6cbe7fe01368240d6cf7c83bba3ebfac45cf2
LeeJaesung2 <dlwotjd9909@naver.com>
2022-09-27 20:22:19 +0900

    merge

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from festival import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name='home'),
+    path('',views.dday, name='dday'),
+    path('home',views.home, name='home'),
     path('festival/', include('festival.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
